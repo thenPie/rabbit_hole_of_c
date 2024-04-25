efa13593d2731b5e344d9391bb3688982726a130
thenPie <118397975+thenPie@users.noreply.github.com>
2024-04-26 02:34:15 +0300

    learning 1

--- src/com_l_args.c
+++ src/com_l_args.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    printf("%s\n", argv[0]);
-    return 0;
+  for (int i = 1; i < argc; i++) {
+    printf("%s\n", argv[i]);
+  }
+  return 0;
 }
