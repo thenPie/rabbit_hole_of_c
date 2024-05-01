5e6655a473bc07d48e8af5a24ba407cb04036a6b
Cherly Marilu <cherlyma@mi-j5.msk.21-school.ru>
2024-05-01 14:37:20 +0300

    flags work but do not cahnge anything

--- src/cat/cat_func.c
+++ src/cat/cat_func.c
@@ -1,7 +1,7 @@
 #include "cat_func.h"
 
-#include <getopt.h>
 #include <stdio.h>
+#include <getopt.h>
 #include <unistd.h>
 
 void cat(int argc, char** argv) {
@@ -11,18 +11,18 @@ void cat(int argc, char** argv) {
 }
 
 void cat_in_action(int argc, char** argv) {
-  print_args(argc, argv);
+  // print_args(argc, argv);
 
-  int number_non_empty_lines_b = 1;  // number-nonblank
-  int show_dollar_ends_e = 1;
-  int number_all_lines_n = 1;      // number
-  int suppress_empty_lines_s = 1;  // squeeze-blank
-  int show_tabs_t = 1;
+  int number_non_empty_lines_b = -1;  // number-nonblank
+  int show_dollar_ends_e = -1;
+  int number_all_lines_n = -1;      // number
+  int suppress_empty_lines_s = -1;  // squeeze-blank
+  int show_tabs_t = -1;
 
   struct option long_opt[] =
   { {"number-nonblank", 0, 0, 'b'},
     {"number", 0, 0, 'n'},
-    {"squeeze-blank", 0, 0, 's'} }
+    {"squeeze-blank", 0, 0, 's'} };
 
   // int b_f = 1; // нумерует только непустые строки
   // int e_f = 1; // \n отображает как $
@@ -30,50 +30,82 @@ void cat_in_action(int argc, char** argv) {
   // int s_f = 1; // сжимает смежные пустые строки
   // int t_f = 1; // отображает табы как ^I
 
-  // FILE* file = fopen(argv[optind], "r");
-  // if (file != NULL) {
-  //   output(file, argc, argv, long_opt, number_non_empty_lines_b,
-  //          show_dollar_ends_e, number_all_lines_n, suppress_empty_lines_s,
-  //          show_tabs_t);
-  // }
-  // fclose(file);
-  FILE* file = fopen(argv[optind], "r");
-  fclose(file);
-}
-
-void output(FILE* file, int argc, char** argv, struct option long_opt[3], int b,
-            int e, int n, int s, int t) {
   int opt;
-  while ((opt = getopt_long(argc, argv, "benstET", long_opt, NULL)) != -1) {
+  while ((opt = getopt_long (argc, argv, "benstET", long_opt, NULL)) != -1) {
     switch (opt) {
       case 'b':
+        printf("%c is option\n", (char)opt);
+        number_non_empty_lines_b = 0;
         break;
       case 'e':
+        show_dollar_ends_e = 0;
         break;
       case 'n':
+        number_all_lines_n = 0;
         break;
       case 's':
+        suppress_empty_lines_s = 0;
         break;
       case 't':
+        show_tabs_t = 0;
         break;
       case 'E':
+        show_dollar_ends_e = 0;
         break;
       case 'T':
+        show_tabs_t = 0;
+        break;
+      default:
         break;
     }
   }
 
-  char c;
-  while ((c = fgetc(file)) != EOF) {
-    printf("%c", c);
+  for (int i = 1; i < argc; i++) {
+    FILE* file = fopen(argv[i], "r");
+    if (file != NULL) {
+      char c;
+      while ((c = fgetc(file)) != EOF) {
+        printf("%c", c);
+      }
+    }
+    fclose(file);
   }
 }
+
+// void output(FILE* file, int argc, char** argv, struct option long_opt[3], int b,
+//             int e, int n, int s, int t) {
+//   int opt;
+//   while ((opt = getopt_long(argc, argv, "benstET", long_opt, NULL)) != -1) {
+//     switch (opt) {
+//       case 'b':
+//         printf("%s - is option\n", (char)opt);
+//         break;
+//       case 'e':
+//         break;
+//       case 'n':
+//         break;
+//       case 's':
+//         break;
+//       case 't':
+//         break;
+//       case 'E':
+//         break;
+//       case 'T':
+//         break;
+//     }
+//   }
+
+//   char c;
+//   while ((c = fgetc(file)) != EOF) {
+//     printf("%c", c);
+//   }
+// }
 
 // getopt, getopt_long, getopt_long_only, optarg, optind, opterr,
 // optopt - Parse command-line options
 
 void print_args(int argc, char** argv) {
-  printf("%d - is argc\n", argc);
+  printf("%d - is argc\n", argc - 1);
   for (int i = 0; i < argc; i++) {
     printf("argv[%d] = %s\n", i, argv[i]);
   }
--- src/cat/cat_func.h
+++ src/cat/cat_func.h
@@ -2,11 +2,12 @@
 #define CAT_FUNC_H
 
 #include <stdio.h>
+#include <getopt.h>
 
 void cat(int argc, char** argv);
 void cat_in_action(int argc, char** argv);
 
-void output(FILE* file, int argc, char** argv, int b, int e, int n, int s, int t);
+// void output(FILE* file, int argc, char** argv, struct option long_opt[], int b, int e, int n, int s, int t);
 
 void print_args(int argc, char** argv);
 
